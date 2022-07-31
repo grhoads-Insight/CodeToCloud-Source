ce1ffa0cc29afdc084de6b3fb2d8e62901e13230
grhoads-Insight <107217628+grhoads-Insight@users.noreply.github.com>
2022-07-31 14:59:03 +0000

    infrastructure creation

---
+++ infrastructure/deploy-infrastructure.ps1
@@ -0,0 +1,25 @@
+#declaring usefull variables for infrastructure deployment
+
+#prefix for a unique identifier on resource names
+$studentprefix = "GDMR"
+#names for resources
+$resourcegroupName = "fabmedical-rg"
+$cosmosDBName = "fabmedical-cdb-"
+$webappName = "fabmedical-web-"
+$planName = "fabmedical-plan-"
+#defining regions (including it's region pair)
+$location1 = "eastus2"
+#easus2 region pair is centralus to ensure highest availability
+$location2 = "centralus"
+
+# Azure CLI commands
+#creating resource group in location1 with given name (does not need to be unique)
+az group create -l $location1 -n $resourcegroupName
+#create a cosmosDB with 2 failover locations with a MongoDB api, backtick used for multi-line command
+az cosmosDB create `
+--name $cosmosDBName `
+--resource-group $resourcegroupName `
+--locations regionName =$location1 failoverPriority=0 isZoneRedundant=False `
+--locations regionName =$location2 failoverPriority=0 isZoneRedundant=True `
+--enable-multiple-write-locations `
+--kind MongoDB
