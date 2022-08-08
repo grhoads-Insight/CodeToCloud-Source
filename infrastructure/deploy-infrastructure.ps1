fe491855ca8eb2c59a6e14b8140338c7b5ff6b79
grhoads-Insight <107217628+grhoads-Insight@users.noreply.github.com>
2022-08-08 13:55:57 +0000

    creating github pipeline

--- infrastructure/deploy-infrastructure.ps1
+++ infrastructure/deploy-infrastructure.ps1
@@ -16,7 +16,7 @@ $appInsights = "fabmedical-ai"
 
 # Azure CLI commands
 #creating resource group in location1 with given name (does not need to be unique)
-az group create -l $location1 -n $resourcegroupName
+az group create -l $location1 -n $resourcegroupName | ConvertFrom-Json
 #create a cosmosDB with 2 failover locations with a MongoDB api, backtick used for multi-line command
 az cosmosDB create --name $cosmosDBName `
 --resource-group $resourcegroupName `
@@ -29,10 +29,17 @@ az appservice plan create --name $planName `
 --resource-group $resourcegroupName `
 --sku S1 `
 --is-linux
+
+az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_URL="https://ghcr.io" --name $($webappName) --resource-group $($resourcegroupName)
+az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_USERNAME="notapplicable" --name $($webappName) --resource-group $($resourcegroupName)
+az webapp config appsettings set --settings DOCKER_REGISTRY_SERVER_PASSWORD="$($env:CR_PAT)" --name $($webappName) --resource-group $($resourcegroupName)
+
 #create the Web App with a single NGINX container
-az webapp create --resource-group $resourcegroupName `
---plan $planName `
---name $webappName `
+az webapp create --resource-group $($resourcegroupName) `
+--multicontainer-config-file ./docker-compose.yml `
+--multicontainer-config-type COMPOSE `
+--plan $($planName) `
+--name $($webappName) `
 -i NGINX
 
 #commands for setting up application insights on azure
@@ -41,11 +48,13 @@ az webapp create --resource-group $resourcegroupName `
 
 
 #keeping commands in here for reference to update the website on azure
-#az webapp config container set `
-#--docker-registry-server-password personal access token `
-#--docker-registry-server-url https://ghcr.io `
-#--docker-registry-server-user notapplicable `
-#--multicontainer-config-file /workspaces/CodeToCloud-Source/docker-compose.yml `
-#--multicontainer-config-type COMPOSE `
-#--name $webappName `
-#--resource-group $resourcegroupName 
+az webapp config container set `
+--docker-registry-server-password $($env:CR_PAT) `
+--docker-registry-server-url https://ghcr.io `
+--docker-registry-server-user notapplicable `
+--multicontainer-config-file ./docker-compose.yml `
+--multicontainer-config-type COMPOSE `
+--name $webappName `
+--resource-group $resourcegroupName
+
+az monitor app-insights component create --app $appInsights --location $location1 --kind web -g $resourcegroupName --application-type web --retention-time 120
